e09ea60f9978f1ddf4c4ace1b6bf788ac299d896
jeonbyeongmin <qudals7613@gmail.com>
2021-12-26 17:14:29 +0900

    STYLE :: Modify logo's width

---
+++ src/components/atoms/Logo.tsx
@@ -0,0 +1,25 @@
+import React, { FunctionComponent } from 'react'
+import styled from '@emotion/styled'
+import { GatsbyImage, IGatsbyImageData } from 'gatsby-plugin-image'
+
+type LogoImageProps = {
+  logoImage: IGatsbyImageData
+}
+
+const Jeondev = styled(GatsbyImage)`
+  width: 108px;
+`
+
+const ImageWrapper = styled.div`
+  padding: 30px;
+`
+
+const Logo: FunctionComponent<LogoImageProps> = ({ logoImage }) => {
+  return (
+    <ImageWrapper>
+      <Jeondev image={logoImage} alt="JeonDev Logo" />
+    </ImageWrapper>
+  )
+}
+
+export default Logo
