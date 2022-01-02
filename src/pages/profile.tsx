2e0f06d48719dcd3ade6112779fcaf3f361fb651
jeonbyeongmin <qudals7613@gmail.com>
2022-01-02 10:51:43 +0900

    FEAT :: profile 페이지 요소 추가

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,3 +1,5 @@
+import styled from '@emotion/styled'
+import SocialIcons from 'components/molecules/SocialIcons'
 import { graphql } from 'gatsby'
 import { GatsbyImage, IGatsbyImageData } from 'gatsby-plugin-image'
 import React, { FunctionComponent } from 'react'
@@ -22,6 +24,59 @@ type ProfilePageProps = {
   }
 }
 
+const ProfileWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 50px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  max-width: 1044px;
+  margin: 0 auto;
+  padding: 50px 40px;
+  width: 100%;
+
+  @media (max-width: 767px) {
+    grid-template-columns: 1fr;
+    width: 100%;
+    grid-gap: 70px;
+  }
+`
+
+const ProfileImageWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const InfoWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  @media (max-width: 767px) {
+    font-size: 15px;
+    font-weight: 300;
+  }
+`
+
+const InfoTitle = styled.div`
+  font-size: 30px;
+  font-weight: 700;
+  margin-bottom: 30px;
+
+  @media (max-width: 767px) {
+    font-size: 25px;
+    font-weight: 500;
+    margin-bottom: 15px;
+  }
+`
+
+const InfoDescription = styled.div`
+  margin-bottom: 50px;
+`
+
 const ProfilePage: FunctionComponent<ProfilePageProps> = ({
   data: {
     site: {
@@ -40,8 +95,22 @@ const ProfilePage: FunctionComponent<ProfilePageProps> = ({
       description={description}
       url={siteUrl}
     >
-      <GatsbyImage image={profileImage} alt="profile" />
-      <div>about page</div>
+      <ProfileWrapper>
+        <ProfileImageWrapper>
+          <GatsbyImage image={profileImage} alt="profile" />
+        </ProfileImageWrapper>
+
+        <InfoWrapper>
+          <InfoTitle>안녕하세요. 전병민입니다.</InfoTitle>
+
+          <InfoDescription>
+            <p>프론트 엔드 개발자가 되기 위해 항상 성장에 대해 고민합니다.</p>
+            <p>동료에게 더 편한 코드를 작성하기 위해 노력합니다.</p>
+            <p>사용자에게 편리함을 주기 위해 노력합니다.</p>
+          </InfoDescription>
+          <SocialIcons />
+        </InfoWrapper>
+      </ProfileWrapper>
     </Layout>
   )
 }
@@ -58,7 +127,7 @@ export const getProfile = graphql`
 
     profile: file(name: { eq: "profile-image" }) {
       childImageSharp {
-        gatsbyImageData(width: 120, height: 120)
+        gatsbyImageData(width: 350, height: 350)
       }
       publicURL
     }
